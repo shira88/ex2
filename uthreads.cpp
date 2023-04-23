0f32a4bea9323af3121ca823387205f978debb06
shiras <shira.schattner@mail.huji.ac.il>
2023-04-23 17:21:53 +0300

    passed test 1

--- uthreads.cpp
+++ uthreads.cpp
@@ -7,13 +7,15 @@
 #include <map>
 #include <set>
 #include <algorithm>
-#include <functional>
 #include "uthreads.h"
 
 // ------------------------------ DEFINITIONS ------------------------------ //
 
 #define JB_SP 6
 #define JB_PC 7
+
+#define AVAILABLE 1
+#define UNAVAILABLE -1
 
 typedef unsigned long address_t;
 
@@ -23,7 +25,7 @@ struct sigaction sa_timer = {0};
 struct itimerval clock_timer;
 struct sigaction sa_clock = {0};
 sigjmp_buf env[MAX_THREAD_NUM];
-bool available_ids[MAX_THREAD_NUM] = {false};
+int available_ids[MAX_THREAD_NUM];
 char *stacks[MAX_THREAD_NUM];
 int quantums[MAX_THREAD_NUM] = {0};
 int running_thread;
@@ -38,7 +40,7 @@ void jump_to_thread ()
 {
   if (setitimer (ITIMER_VIRTUAL, &running_timer, nullptr))
   {
-    std::cout << "system error: setitimer error." << std::endl;
+    std::cerr << "system error: setitimer error." << std::endl;
   }
 
   sigsetjmp(env[running_thread], 1);
@@ -49,14 +51,17 @@ void jump_to_thread ()
 
   quantums[running_thread]++;
 
+  std::cout << "running thread: " << running_thread << std::endl;
+
   siglongjmp (env[running_thread], 1);
 }
 
 int get_tid ()
 {
-  for (int i = 0; i < MAX_THREAD_NUM; i++)
+  for (int i = 1; i < MAX_THREAD_NUM; i++)
   {
-    if (available_ids[i])
+    int status = available_ids[i];
+    if (status == AVAILABLE)
     {
       return i;
     }
@@ -74,17 +79,18 @@ address_t translate_address (address_t addr)
   return ret;
 }
 
-int is_valid_tid (int tid)
+bool is_valid_tid (int tid)
 {
-  if ((tid < 0) | (tid >= MAX_THREAD_NUM) | (available_ids[tid]))
+  if ((tid < 0) | (tid >= MAX_THREAD_NUM) | (available_ids[tid] == AVAILABLE))
   {
-    return -1;
+    return false;
   }
-  return 0;
+  return true;
 }
 
 void timer_handler (int sig)
 {
+  ready_deque.push_back(running_thread);
   jump_to_thread ();
 }
 
@@ -116,16 +122,21 @@ void clock_handler (int sig)
 
 int uthread_init (int quantum_usecs)
 {
-  total_quantums = 0;
-
+  if(quantum_usecs <= 0) {
+    std::cerr << "thread library error: quantum should be a positive number" << std::endl;
+    return -1;
+  }
   running_thread = 0;
-  available_ids[0] = true;
+  available_ids[0] = UNAVAILABLE;
+  for(int i = 1; i < MAX_THREAD_NUM; i++) {
+    available_ids[i] = AVAILABLE;
+  }
 
   // create a running_timer for the running state
   sa_timer.sa_handler = &timer_handler;
   if (sigaction (SIGVTALRM, &sa_timer, nullptr) < 0)
   {
-    std::cout << "system error: sigaction error." << std::endl;
+    std::cerr << "system error: sigaction error." << std::endl;
   }
 
   running_timer.it_interval.tv_sec = 0;
@@ -134,13 +145,13 @@ int uthread_init (int quantum_usecs)
   // Start a virtual running_timer. It counts down whenever this process is executing.
   if (setitimer (ITIMER_VIRTUAL, &running_timer, nullptr))
   {
-    std::cout << "system error: setitimer error." << std::endl;
+    std::cerr << "system error: setitimer error." << std::endl;
   }
 
   sa_clock.sa_handler = &clock_handler;
   if (sigaction (SIGVTALRM, &sa_clock, nullptr) < 0)
   {
-    std::cout << "system error: sigaction error." << std::endl;
+    std::cerr << "system error: sigaction error." << std::endl;
   }
 
   clock_timer.it_interval.tv_sec = 0;
@@ -149,11 +160,11 @@ int uthread_init (int quantum_usecs)
   // Start a virtual running_timer. It counts down whenever this process is executing.
   if (setitimer (ITIMER_VIRTUAL, &clock_timer, nullptr))
   {
-    std::cout << "system error: setitimer error." << std::endl;
+    std::cerr << "system error: setitimer error." << std::endl;
   }
 
   sigaddset(&env[running_thread]->__saved_mask, SIGVTALRM);
-
+  total_quantums = 1;
 
   return 0;
 }
@@ -164,21 +175,21 @@ int uthread_spawn (thread_entry_point entry_point)
 
   if (entry_point == nullptr)
   {
-    std::cout << "thread library error: NULL entry point" << std::endl;
+    std::cerr << "thread library error: NULL entry point" << std::endl;
     return -1;
   }
 
   int tid = get_tid ();
   if (tid == -1)
   {
-    std::cout
+    std::cerr
         << "thread library error: The number of concurrent threads exceeded the limit"
         << std::endl;
     return -1;
   }
 
   ready_deque.push_back (tid);
-  available_ids[tid] = false;
+  available_ids[tid] = UNAVAILABLE;
 
   char *stack = new char[STACK_SIZE];
 
@@ -205,16 +216,15 @@ int uthread_terminate (int tid)
 
   if (is_valid_tid (tid))
   {
-    available_ids[tid] = false;
+    available_ids[tid] = AVAILABLE;
   }
   else
   {
-    std::cout << "thread library error: No such thread" << std::endl;
+    std::cerr << "thread library error: No such thread" << std::endl;
     return -1;
   }
 
-  // check if the tid is in the ready queue or the blocked, and if so
-  // removes it
+  // check if the tid is in the ready queue or the blocked, and if so removes it
 
   if (blocked_set.find (tid) != blocked_set.end ())
   {
@@ -246,13 +256,13 @@ int uthread_block (int tid)
   //CHECKS IF THE TID IS EVEN VALID
   if (!is_valid_tid (tid))
   {
-    std::cout << "thread library error: invalid tid\n";
+    std::cerr << "thread library error: invalid tid" << std::endl;
     return -1;
   }
   //CHECKS IF IT ISN'T ALREADY BLOCKED
   if (blocked_set.find (tid) != blocked_set.end ())
   {
-    std::cout << "thread library error: tid is already blocked\n";
+    std::cerr << "thread library error: tid is already blocked" << std::endl;
     return -1;
   }
   blocked_set.insert (tid);
@@ -277,6 +287,7 @@ int uthread_resume (int tid)
 
   if (blocked_set.find (tid) == blocked_set.end ())
   {
+    std::cerr << "The provided tid doesn't belong to a blocked thread." << std::endl;
     return -1;
   }
   blocked_set.erase (tid);
@@ -293,7 +304,7 @@ int uthread_sleep (int num_quantums)
 
   if (running_thread == 0)
   {
-    std::cout << "Cannot call uthread_sleep from the main thread";
+    std::cerr << "Cannot call uthread_sleep from the main thread" << std::endl;
     return -1;
   }
 
